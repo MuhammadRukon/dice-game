212cd0eaaa728f2aa302b8a0acbcdfb6b5cd5fc5
MuhammadRukon <muhammad.rukon619@gmail.com>
2021-08-11 00:23:35 +0600

    complete

--- script.js
+++ script.js
@@ -102,5 +102,23 @@ btnNew.addEventListener('click', init);
 // '''''''''''''
 // //////////
 ///////
+// rule modal
+///////////////
 
-////////////////
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnOpenModal.addEventListener('click', function () {
+  console.log('btn clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
